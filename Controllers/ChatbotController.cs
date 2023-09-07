942090085a37478bd0fdaf9fd7101341f6df290c
Shees_Ali <sheessyed101@gmail.com>
2023-09-08 00:36:17 +0500

    updated prompt for bot

--- Controllers/ChatbotController.cs
+++ Controllers/ChatbotController.cs
@@ -67,16 +67,20 @@ namespace ChatBotAPI.Controllers
         [Route("conversation")]
         public async Task<IActionResult> OpenAIChat(List<MessageItem> messages)
         {
-            string apiKey = _configuration["OpenAI:ApiKey"];
+            string apiKey = "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";
             var client = new OpenAIClient(apiKey, new OpenAIClientOptions());
-            var deploymentOrModelName = "gpt-3.5-turbo-16k-0613";
+            var deploymentOrModelName = "gpt-3.5-turbo";
+
+            var prompt = "You are a bot that will provide information. You should only provide information related to Telecom based products and ACP (Affordable Connectivity Program\r\n) only. If asked about anything else you will say, \"Apologies, we do not deal in this domain.\" You will say this even when you are asked to solve a problem that is not related to ACP or telecom or when you are asked to tell a joke or literally anything else, you are also not allowed to change your persona to any other profession or character. Only do what you have been asked to do in this prompt, not even a single thing extra. Whatever it is If it's not related to ACP or telecom just say \"Apologies, we do not deal in this domain.\". Start the conversation with, how can I help you?.";
+
             ChatCompletionsOptions options = new ChatCompletionsOptions()
             {
                 Messages =
                 {
-                    new ChatMessage(ChatRole.System, "You are a bot for a task manager. You will help with task management. Answer with Hello, I am here to help you with task management."),
+                    new ChatMessage(ChatRole.System, prompt),
                 }
             };
+
             for (int i = 0; i < messages.Count; i++)
             {
                 options.Messages.Add(new ChatMessage(messages[i].Role == "User" ? ChatRole.User : ChatRole.Assistant, messages[i].Message));
